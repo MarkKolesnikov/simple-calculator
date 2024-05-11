06967120c50c3e675914be4eb31266bd5b77ea64
Your Name <azzytm@yandex.ru>
2024-05-12 00:25:30 +0300

    Create a panel capture

--- src/main/MainWindowsController.java
+++ src/main/MainWindowsController.java
@@ -1,13 +1,27 @@
 package main;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainWindowsController implements Initializable {
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+public class MainWindowsController {
+    @FXML private Pane titlePane;
 
+    private double x, y;
+
+    public void init(Stage stage) {
+        titlePane.setOnMousePressed(mouseEvent -> {
+            x = mouseEvent.getSceneX();
+            y = mouseEvent.getSceneY();
+        });
+        titlePane.setOnMouseDragged(mouseEvent -> {
+            stage.setX(mouseEvent.getScreenX()-x);
+            stage.setY(mouseEvent.getScreenY()-y);
+        });
     }
 }
